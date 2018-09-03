883520f21739311a124ca40f57e729dce883249f
Druck Service <Marina@workds.local>
2018-09-03 10:51:43 +0300

    First commit

---
+++ admin/post-edit.php
@@ -0,0 +1,38 @@
+<pre><?php print_r($_POST); ?></pre>
+  <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+if (!empty ($_POST)){
+    require_once('../includes/class-insert.php');
+    
+    if ($insert->post($_POST)){
+        echo'<p>Data inserted successfully!';
+    }
+}
+?>
+   <html>
+    <head>
+        <title>Insert post</title>
+    </head>
+    <body>
+       <form method="post">
+        <p>
+            <input type="text" name="post_title" />
+        </p>
+        <p>
+            <textarea name="post_content"></textarea>
+        </p>
+        <p>
+        <label for="post_category">Cat1</label>
+        <input type="checkbox" name="post_category[first]" value="cat1" />
+        <br />
+        <label for="post_category">Cat2</label>
+        <input type="checkbox" name="post_category[second]" value="cat2" />
+        </p>
+        <p>
+            <input type="submit" value="Submit" />
+        </p>
+        </form>
+    </body>
+</html>
